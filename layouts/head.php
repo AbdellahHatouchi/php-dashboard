3417fbff08dd4bc1e59e52f77fe23dbb37581f52
abdellah <abdel.hat19@gmail.com>
2023-05-13 18:21:09 +0100

    v1

---
+++ layouts/head.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--use bootstrap cdn css and js -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Option 1: Include in HTML icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
---
+++ otherpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('layouts/head.php') ?>
+    <title>home</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <?php include('layouts/sidebar.php') ?>
+            <div class="col px-4 py-3">
+                <p>this is content of other page</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
